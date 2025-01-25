5445a3e4259b316a79da8397dd48cfc93cb0fce3
fbsmith <fletcherbsmith@gmail.com>
2025-01-24 17:34:15 -0700

    Passed all testsgit add .!

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -19,14 +19,29 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove upMove = new ChessMove(myPosition, upPosition);
 
         if (isInBounds(upPosition) && !isBlocked(board, myPosition, upPosition) && !canCapture(board, myPosition, upPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            possibleMoves.add(upMove);
+            if (currentRow==7) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(upMove);
+            }
         }
 
         // pawns can move up twice if white and second row
         ChessPosition upUpPosition = new ChessPosition(currentRow+2, currentCol);
         ChessMove upUpMove = new ChessMove(myPosition, upUpPosition);
 
-        if (isInBounds(upUpPosition) && !isBlocked(board, myPosition, upUpPosition) && !canCapture(board, myPosition, upUpPosition) && (myTeam == ChessGame.TeamColor.WHITE) && (currentRow == 2)) {
+        if (isInBounds(upUpPosition) && !isBlocked(board, myPosition, upPosition) && !isBlocked(board, myPosition, upUpPosition) && !canCapture(board, myPosition, upUpPosition) && (myTeam == ChessGame.TeamColor.WHITE) && (currentRow == 2)) {
             possibleMoves.add(upUpMove);
         }
 
@@ -35,14 +50,29 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove downMove = new ChessMove(myPosition, downPosition);
 
         if (isInBounds(downPosition) && !isBlocked(board, myPosition, downPosition) && !canCapture(board, myPosition, downPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            possibleMoves.add(downMove);
+            if (currentRow==2) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(downMove);
+            }
         }
 
         // pawns can move down twice if black and seventh row
         ChessPosition downDownPosition = new ChessPosition(currentRow-2, currentCol);
         ChessMove downDownMove = new ChessMove(myPosition, downDownPosition);
 
-        if (isInBounds(downDownPosition) && !isBlocked(board, myPosition, downDownPosition) && !canCapture(board, myPosition, downDownPosition) && (myTeam == ChessGame.TeamColor.BLACK) && (currentRow == 7)) {
+        if (isInBounds(downDownPosition) && !isBlocked(board, myPosition, downPosition) && !isBlocked(board, myPosition, downDownPosition) && !canCapture(board, myPosition, downDownPosition) && (myTeam == ChessGame.TeamColor.BLACK) && (currentRow == 7)) {
             possibleMoves.add(downDownMove);
         }
 
@@ -51,7 +81,22 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove upRightMove = new ChessMove(myPosition, upRightPosition);
 
         if (isInBounds(upRightPosition) && !isBlocked(board, myPosition, upRightPosition) && canCapture(board, myPosition, upRightPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            possibleMoves.add(upRightMove);
+            if (currentRow==7) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(upRightMove);
+            }
         }
 
         // pawns can capture down and to the right if black
@@ -59,7 +104,22 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove downRightMove = new ChessMove(myPosition, downRightPosition);
 
         if (isInBounds(downRightPosition) && !isBlocked(board, myPosition, downRightPosition) && canCapture(board, myPosition, downRightPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            possibleMoves.add(downRightMove);
+            if (currentRow==2) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(downRightMove);
+            }
         }
 
         // pawns can capture up and to the left if white
@@ -67,7 +127,22 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove upLeftMove = new ChessMove(myPosition, upLeftPosition);
 
         if (isInBounds(upLeftPosition) && !isBlocked(board, myPosition, upLeftPosition) && canCapture(board, myPosition, upLeftPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            possibleMoves.add(upLeftMove);
+            if (currentRow==7) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(upLeftMove);
+            }
         }
 
         // pawns can capture down and to the left if black
@@ -75,7 +150,22 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         ChessMove downLeftMove = new ChessMove(myPosition, downLeftPosition);
 
         if (isInBounds(downLeftPosition) && !isBlocked(board, myPosition, downLeftPosition) && canCapture(board, myPosition, downLeftPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            possibleMoves.add(downLeftMove);
+            if (currentRow==2) {
+                ChessMove bishopPromotionMove = new ChessMove(myPosition, downLeftPosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(bishopPromotionMove);
+
+                ChessMove knightPromotionMove = new ChessMove(myPosition, downLeftPosition, ChessPiece.PieceType.KNIGHT);
+                possibleMoves.add(knightPromotionMove);
+
+                ChessMove rookPromotionMove = new ChessMove(myPosition, downLeftPosition, ChessPiece.PieceType.ROOK);
+                possibleMoves.add(rookPromotionMove);
+
+                ChessMove queenPromotionMove = new ChessMove(myPosition, downLeftPosition, ChessPiece.PieceType.QUEEN);
+                possibleMoves.add(queenPromotionMove);
+            }
+            else {
+                possibleMoves.add(downLeftMove);
+            }
         }
 
         return possibleMoves;

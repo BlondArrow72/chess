24c5ed67f3a054655a6319b7a984ed2921166293
fbsmith <fletcherbsmith@gmail.com>
2025-03-11 23:15:54 -0600

    Cleaned up a little more of the PawnMovesCalculator

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -5,7 +5,12 @@ import java.util.Collection;
 import java.util.Arrays;
 
 public class PawnMovesCalculator implements ChessPieceMovesCalculator{
+    private ChessBoard board;
+    private ChessPosition myPosition;
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
 
         Collection<ChessMove> possibleMoves = new ArrayList<>();
 
@@ -21,17 +26,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(upPosition) && !isBlocked(board, myPosition, upPosition) && !canCapture(board, myPosition, upPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
             if (currentRow==7) {
-                ChessMove bishopPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.BISHOP);
-                possibleMoves.add(bishopPromotionMove);
-
-                ChessMove knightPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.KNIGHT);
-                possibleMoves.add(knightPromotionMove);
-
-                ChessMove rookPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.ROOK);
-                possibleMoves.add(rookPromotionMove);
-
-                ChessMove queenPromotionMove = new ChessMove(myPosition, upPosition, ChessPiece.PieceType.QUEEN);
-                possibleMoves.add(queenPromotionMove);
+                possibleMoves.addAll(getPromotionMoves(upPosition));
             }
             else {
                 possibleMoves.add(upMove);
@@ -52,17 +47,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(downPosition) && !isBlocked(board, myPosition, downPosition) && !canCapture(board, myPosition, downPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
             if (currentRow==2) {
-                ChessMove bishopPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.BISHOP);
-                possibleMoves.add(bishopPromotionMove);
-
-                ChessMove knightPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.KNIGHT);
-                possibleMoves.add(knightPromotionMove);
-
-                ChessMove rookPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.ROOK);
-                possibleMoves.add(rookPromotionMove);
-
-                ChessMove queenPromotionMove = new ChessMove(myPosition, downPosition, ChessPiece.PieceType.QUEEN);
-                possibleMoves.add(queenPromotionMove);
+                possibleMoves.addAll(getPromotionMoves(downPosition));
             }
             else {
                 possibleMoves.add(downMove);
@@ -83,17 +68,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(upRightPosition) && !isBlocked(board, myPosition, upRightPosition) && canCapture(board, myPosition, upRightPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
             if (currentRow==7) {
-                ChessMove bishopPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.BISHOP);
-                possibleMoves.add(bishopPromotionMove);
-
-                ChessMove knightPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.KNIGHT);
-                possibleMoves.add(knightPromotionMove);
-
-                ChessMove rookPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.ROOK);
-                possibleMoves.add(rookPromotionMove);
-
-                ChessMove queenPromotionMove = new ChessMove(myPosition, upRightPosition, ChessPiece.PieceType.QUEEN);
-                possibleMoves.add(queenPromotionMove);
+                possibleMoves.addAll(getPromotionMoves(upRightPosition));
             }
             else {
                 possibleMoves.add(upRightMove);
@@ -106,17 +81,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(downRightPosition) && !isBlocked(board, myPosition, downRightPosition) && canCapture(board, myPosition, downRightPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
             if (currentRow==2) {
-                ChessMove bishopPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.BISHOP);
-                possibleMoves.add(bishopPromotionMove);
-
-                ChessMove knightPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.KNIGHT);
-                possibleMoves.add(knightPromotionMove);
-
-                ChessMove rookPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.ROOK);
-                possibleMoves.add(rookPromotionMove);
-
-                ChessMove queenPromotionMove = new ChessMove(myPosition, downRightPosition, ChessPiece.PieceType.QUEEN);
-                possibleMoves.add(queenPromotionMove);
+                possibleMoves.addAll(getPromotionMoves(downRightPosition));
             }
             else {
                 possibleMoves.add(downRightMove);
@@ -129,17 +94,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(upLeftPosition) && !isBlocked(board, myPosition, upLeftPosition) && canCapture(board, myPosition, upLeftPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
             if (currentRow==7) {
-                ChessMove bishopPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.BISHOP);
-                possibleMoves.add(bishopPromotionMove);
-
-                ChessMove knightPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.KNIGHT);
-                possibleMoves.add(knightPromotionMove);
-
-                ChessMove rookPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.ROOK);
-                possibleMoves.add(rookPromotionMove);
-
-                ChessMove queenPromotionMove = new ChessMove(myPosition, upLeftPosition, ChessPiece.PieceType.QUEEN);
-                possibleMoves.add(queenPromotionMove);
+                possibleMoves.addAll(getPromotionMoves(upLeftPosition));
             }
             else {
                 possibleMoves.add(upLeftMove);
@@ -152,7 +107,7 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
 
         if (isInBounds(downLeftPosition) && !isBlocked(board, myPosition, downLeftPosition) && canCapture(board, myPosition, downLeftPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
             if (currentRow==2) {
-                possibleMoves = addPromotionMoves(possibleMoves, myPosition, downLeftPosition);
+                possibleMoves.addAll(getPromotionMoves(downLeftPosition));
             }
             else {
                 possibleMoves.add(downLeftMove);
@@ -162,7 +117,10 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
         return possibleMoves;
     }
 
-    private Collection<ChessMove> addPromotionMoves(Collection<ChessMove> possibleMoves, ChessPosition currentPosition, ChessPosition promotionPosition) {
+    private Collection<ChessMove> getPromotionMoves(ChessPosition promotionPosition) {
+        // instantiate promotionMoves
+        Collection<ChessMove> promotionMoves = new ArrayList<>();
+
         // promotion types
         Collection<ChessPiece.PieceType> promotionTypes = Arrays.asList(
                 ChessPiece.PieceType.BISHOP,
@@ -171,12 +129,12 @@ public class PawnMovesCalculator implements ChessPieceMovesCalculator{
                 ChessPiece.PieceType.QUEEN
         );
 
-        // add all move types to possibleMoves
+        // add all move types to promotionMoves
         for(ChessPiece.PieceType pieceType : promotionTypes) {
-            ChessMove newMove = new ChessMove(currentPosition, promotionPosition, pieceType);
-            possibleMoves.add(newMove);
+            ChessMove newMove = new ChessMove(myPosition, promotionPosition, pieceType);
+            promotionMoves.add(newMove);
         }
 
-        return possibleMoves;
+        return promotionMoves;
     }
 }

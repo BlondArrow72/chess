d3ac1adcded41fb9acaed4de21cf9c624d106070
fbsmith <fletcherbsmith@gmail.com>
2025-03-14 22:34:22 -0600

    Cleaned up code, should be all good to go!

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -5,112 +5,125 @@ import java.util.Collection;
 import java.util.Arrays;
 
 public class PawnMovesCalculator implements ChessPieceMovesCalculator{
-    private ChessBoard board;
-    private ChessPosition myPosition;
+    ChessBoard board;
+    ChessPosition myPosition;
+    ChessGame.TeamColor teamColor;
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         this.board = board;
         this.myPosition = myPosition;
 
-        Collection<ChessMove> possibleMoves = new ArrayList<>();
+        ChessPiece currentPiece = board.getPiece(myPosition);
+        if (currentPiece == null) {
+            return null;
+        }
+
+        this.teamColor = board.getPiece(myPosition).getTeamColor();
 
         int currentRow = myPosition.getRow();
         int currentCol = myPosition.getColumn();
 
-        ChessPiece myPiece = board.getPiece(myPosition);
-        ChessGame.TeamColor myTeam = myPiece.getTeamColor();
+        // pawns can make regular moves
+        ChessPosition newPosition;
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            newPosition = new ChessPosition(currentRow + 1, currentCol);
+        }
+        else {
+            newPosition = new ChessPosition(currentRow - 1, currentCol);
+        }
+        Collection<ChessMove> possibleMoves = regularMove(newPosition);
 
-        // pawns can move up if white
-        ChessPosition upPosition = new ChessPosition(currentRow+1, currentCol);
-        ChessMove upMove = new ChessMove(myPosition, upPosition);
+        // pawns can move twice from base row
+        ChessPosition enPassantPosition = null;
+        if (teamColor == ChessGame.TeamColor.WHITE && myPosition.getRow() == 2
+                && board.getPiece(new ChessPosition(currentRow + 1, currentCol)) == null) {
+            enPassantPosition = new ChessPosition(currentRow + 2, currentCol);
+        }
+        if (teamColor == ChessGame.TeamColor.BLACK && myPosition.getRow() == 7
+                && board.getPiece(new ChessPosition(currentRow - 1, currentCol)) == null) {
+            enPassantPosition = new ChessPosition(currentRow - 2, currentCol);
+        }
+        if (enPassantPosition != null) {
+            possibleMoves.addAll(regularMove(enPassantPosition));
+        }
 
-        if (isInBounds(upPosition) && !isBlocked(board, myPosition, upPosition) && !canCapture(board, myPosition, upPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            if (currentRow==7) {
-                possibleMoves.addAll(getPromotionMoves(upPosition));
+        // pawns can capture
+        Collection<ChessPosition> capturePositions = new ArrayList<>();
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            capturePositions.add(new ChessPosition(currentRow + 1, currentCol - 1));
+            capturePositions.add(new ChessPosition(currentRow + 1, currentCol + 1));
+        }
+        else {
+            capturePositions.add(new ChessPosition(currentRow - 1, currentCol - 1));
+            capturePositions.add(new ChessPosition(currentRow - 1, currentCol + 1));
+        }
+        for (ChessPosition capturePosition : capturePositions) {
+            possibleMoves.addAll(captureMove(capturePosition));
+        }
+
+        return possibleMoves;
+    }
+
+    private Collection<ChessMove> regularMove(ChessPosition newPosition) {
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+
+        // move must be in bounds and unoccupied
+        if (isInBounds(newPosition) && board.getPiece(newPosition) == null) {
+            // if white and newPosition is row 8
+            if (teamColor == ChessGame.TeamColor.WHITE) {
+                // get promotionMoves
+                if (newPosition.getRow() == 8) {
+                    possibleMoves.addAll(getPromotionMoves(newPosition));
+                }
+
+                // get regular moves
+                else {
+                    ChessMove newMove = new ChessMove(myPosition, newPosition);
+                    possibleMoves = evaluateMove(possibleMoves, board, newMove);
+                }
             }
+
+            // if black
             else {
-                possibleMoves.add(upMove);
+                // get promotion moves
+                if (newPosition.getRow() == 1) {
+                    possibleMoves.addAll(getPromotionMoves(newPosition));
+                }
+
+                // get regular moves
+                else {
+                    ChessMove newMove = new ChessMove(myPosition, newPosition);
+                    possibleMoves = evaluateMove(possibleMoves, board, newMove);
+                }
             }
         }
 
-        // pawns can move up twice if white and second row
-        ChessPosition upUpPosition = new ChessPosition(currentRow+2, currentCol);
-        ChessMove upUpMove = new ChessMove(myPosition, upUpPosition);
+        return possibleMoves;
+    }
 
-        if (isInBounds(upUpPosition) && !isBlocked(board, myPosition, upPosition) && !isBlocked(board, myPosition, upUpPosition) && !canCapture(board, myPosition, upUpPosition) && (myTeam == ChessGame.TeamColor.WHITE) && (currentRow == 2)) {
-            possibleMoves.add(upUpMove);
-        }
+    private Collection<ChessMove> captureMove(ChessPosition newPosition) {
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
 
-        // pawns can move down if black
-        ChessPosition downPosition = new ChessPosition(currentRow-1, currentCol);
-        ChessMove downMove = new ChessMove(myPosition, downPosition);
+        // capture position must be in bounds, not blocked by your team, and occupied by other team
+        if (isInBounds(newPosition)) {
+            ChessPiece targetPiece = board.getPiece(newPosition);
+            if (targetPiece != null && teamColor != targetPiece.getTeamColor()) {
 
-        if (isInBounds(downPosition) && !isBlocked(board, myPosition, downPosition) && !canCapture(board, myPosition, downPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            if (currentRow==2) {
-                possibleMoves.addAll(getPromotionMoves(downPosition));
-            }
-            else {
-                possibleMoves.add(downMove);
-            }
-        }
+                // promote if white on last row
+                if (teamColor == ChessGame.TeamColor.WHITE && newPosition.getRow() == 8) {
+                    possibleMoves.addAll(getPromotionMoves(newPosition));
+                }
 
-        // pawns can move down twice if black and seventh row
-        ChessPosition downDownPosition = new ChessPosition(currentRow-2, currentCol);
-        ChessMove downDownMove = new ChessMove(myPosition, downDownPosition);
+                // promote if black on first row
+                else if (teamColor == ChessGame.TeamColor.BLACK && newPosition.getRow() == 1) {
+                    possibleMoves.addAll(getPromotionMoves(newPosition));
+                }
 
-        if (isInBounds(downDownPosition) && !isBlocked(board, myPosition, downPosition) && !isBlocked(board, myPosition, downDownPosition) && !canCapture(board, myPosition, downDownPosition) && (myTeam == ChessGame.TeamColor.BLACK) && (currentRow == 7)) {
-            possibleMoves.add(downDownMove);
-        }
-
-        // pawns can capture up and to the right if white
-        ChessPosition upRightPosition = new ChessPosition(currentRow+1, currentCol+1);
-        ChessMove upRightMove = new ChessMove(myPosition, upRightPosition);
-
-        if (isInBounds(upRightPosition) && !isBlocked(board, myPosition, upRightPosition) && canCapture(board, myPosition, upRightPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            if (currentRow==7) {
-                possibleMoves.addAll(getPromotionMoves(upRightPosition));
-            }
-            else {
-                possibleMoves.add(upRightMove);
-            }
-        }
-
-        // pawns can capture down and to the right if black
-        ChessPosition downRightPosition = new ChessPosition(currentRow-1, currentCol+1);
-        ChessMove downRightMove = new ChessMove(myPosition, downRightPosition);
-
-        if (isInBounds(downRightPosition) && !isBlocked(board, myPosition, downRightPosition) && canCapture(board, myPosition, downRightPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            if (currentRow==2) {
-                possibleMoves.addAll(getPromotionMoves(downRightPosition));
-            }
-            else {
-                possibleMoves.add(downRightMove);
-            }
-        }
-
-        // pawns can capture up and to the left if white
-        ChessPosition upLeftPosition = new ChessPosition(currentRow+1, currentCol-1);
-        ChessMove upLeftMove = new ChessMove(myPosition, upLeftPosition);
-
-        if (isInBounds(upLeftPosition) && !isBlocked(board, myPosition, upLeftPosition) && canCapture(board, myPosition, upLeftPosition) && (myTeam == ChessGame.TeamColor.WHITE)) {
-            if (currentRow==7) {
-                possibleMoves.addAll(getPromotionMoves(upLeftPosition));
-            }
-            else {
-                possibleMoves.add(upLeftMove);
-            }
-        }
-
-        // pawns can capture down and to the left if black
-        ChessPosition downLeftPosition = new ChessPosition(currentRow-1, currentCol-1);
-        ChessMove downLeftMove = new ChessMove(myPosition, downLeftPosition);
-
-        if (isInBounds(downLeftPosition) && !isBlocked(board, myPosition, downLeftPosition) && canCapture(board, myPosition, downLeftPosition) && (myTeam == ChessGame.TeamColor.BLACK)) {
-            if (currentRow==2) {
-                possibleMoves.addAll(getPromotionMoves(downLeftPosition));
-            }
-            else {
-                possibleMoves.add(downLeftMove);
+                // add move if regular capture
+                else {
+                    ChessMove newMove = new ChessMove(myPosition, newPosition);
+                    possibleMoves.add(newMove);
+                }
             }
         }
 

958badc747bdc61d33d8131f801f9c6cc333e6b2
fbsmith <fletcherbsmith@gmail.com>
2025-03-14 21:27:34 -0600

    Simplified KingMovesCalculator and KnightMovesCalculator

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -8,48 +8,19 @@ public class KingMovesCalculator implements ChessPieceMovesCalculator{
 
         Collection<ChessMove> possibleMoves = new ArrayList<>();
 
-        int currentRow = myPosition.getRow();
-        int currentCol = myPosition.getColumn();
+        int[][] directions = {
+                { 1, -1}, { 1, 0}, { 1, 1},
+                { 0, -1},          { 0, 1},
+                {-1, -1}, {-1, 0}, {-1, 1}
+        };
 
-        // kings can move up
-        ChessPosition upPosition = new ChessPosition(currentRow+1, currentCol);
-        ChessMove upMove = new ChessMove(myPosition, upPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upMove);
-
-        // kings can move up and to the right
-        ChessPosition upRightPosition = new ChessPosition(currentRow+1, currentCol+1);
-        ChessMove upRightMove = new ChessMove(myPosition, upRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upRightMove);
-
-        // kings can move to the right
-        ChessPosition rightPosition = new ChessPosition(currentRow, currentCol+1);
-        ChessMove rightMove = new ChessMove(myPosition, rightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, rightMove);
-
-        // kings can move down and to the right
-        ChessPosition downRightPosition = new ChessPosition(currentRow-1, currentCol+1);
-        ChessMove downRightMove = new ChessMove(myPosition, downRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downRightMove);
-
-        // kings can move down
-        ChessPosition downPosition = new ChessPosition(currentRow-1, currentCol);
-        ChessMove downMove = new ChessMove(myPosition, downPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downMove);
-
-        // kings can move down and to the left
-        ChessPosition downLeftPosition = new ChessPosition(currentRow-1, currentCol-1);
-        ChessMove downLeftMove = new ChessMove(myPosition, downLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downLeftMove);
-
-        // kings can move to the left
-        ChessPosition leftPosition = new ChessPosition(currentRow, currentCol-1);
-        ChessMove leftMove = new ChessMove(myPosition, leftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, leftMove);
-
-        // kings can move up and to the left
-        ChessPosition upLeftPosition = new ChessPosition(currentRow+1, currentCol-1);
-        ChessMove upLeftMove = new ChessMove(myPosition, upLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upLeftMove);
+        for (int[] direction : directions) {
+            int newRow = direction[0] + myPosition.getRow();
+            int newCol = direction[1] + myPosition.getColumn();
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessMove newMove = new ChessMove(myPosition, newPosition);
+            possibleMoves = evaluateMove(possibleMoves, board, newMove);
+        }
 
         return possibleMoves;
     }
--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -8,48 +8,20 @@ public class KnightMovesCalculator implements ChessPieceMovesCalculator{
 
         Collection<ChessMove> possibleMoves = new ArrayList<>();
 
-        int currentRow = myPosition.getRow();
-        int currentCol = myPosition.getColumn();
+        int[][] directions = {
+                          { 2, -1}, { 2, 1},
+                { 1, -2},                    { 1, 2},
+                {-1, -2},                    {-1, 2},
+                          {-2, -1}, {-2, 1}
+        };
 
-        // knights can move up 2 and right 1
-        ChessPosition upUpRightPosition = new ChessPosition(currentRow+2, currentCol+1);
-        ChessMove upUpRightMove = new ChessMove(myPosition, upUpRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upUpRightMove);
-
-        // knights can move up 1 and right 2
-        ChessPosition upRightRightPosition = new ChessPosition(currentRow+1, currentCol+2);
-        ChessMove upRightRightMove = new ChessMove(myPosition, upRightRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upRightRightMove);
-
-        // knights can move down 1 and right 2
-        ChessPosition downRightRightPosition = new ChessPosition(currentRow-1, currentCol+2);
-        ChessMove downRightRightMove = new ChessMove(myPosition, downRightRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downRightRightMove);
-
-        // knights can move down 2 and right 1
-        ChessPosition downDownRightPosition = new ChessPosition(currentRow-2, currentCol+1);
-        ChessMove downDownRightMove = new ChessMove(myPosition, downDownRightPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downDownRightMove);
-
-        // knights can move down 2 and left 1
-        ChessPosition downDownLeftPosition = new ChessPosition(currentRow-2, currentCol-1);
-        ChessMove downDownLeftMove = new ChessMove(myPosition, downDownLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downDownLeftMove);
-
-        // knights can move down 1 and left 2
-        ChessPosition downLeftLeftPosition = new ChessPosition(currentRow-1, currentCol-2);
-        ChessMove downLeftLeftMove = new ChessMove(myPosition, downLeftLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, downLeftLeftMove);
-
-        // knights can move up 1 and left 2
-        ChessPosition upLeftLeftPosition = new ChessPosition(currentRow+1, currentCol-2);
-        ChessMove upLeftLeftMove = new ChessMove(myPosition, upLeftLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upLeftLeftMove);
-
-        // knights can move up 2 and left 1
-        ChessPosition upUpLeftPosition = new ChessPosition(currentRow+2, currentCol-1);
-        ChessMove upUpLeftMove = new ChessMove(myPosition, upUpLeftPosition);
-        possibleMoves = evaluateMove(possibleMoves, board, upUpLeftMove);
+        for (int[] direction : directions) {
+            int newRow = direction[0] + myPosition.getRow();
+            int newCol = direction[1] + myPosition.getColumn();
+            ChessPosition newPosition = new ChessPosition(newRow, newCol);
+            ChessMove newMove = new ChessMove(myPosition, newPosition);
+            possibleMoves = evaluateMove(possibleMoves, board, newMove);
+        }
 
         return possibleMoves;
     }

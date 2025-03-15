252767aa465c8b4e13946ea6b360e9d19f1df81c
fbsmith <fletcherbsmith@gmail.com>
2025-03-14 21:22:30 -0600

    Simplified QueenMovesCalculator, RookMovesCalculator, and BishopMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -8,83 +8,13 @@ public class BishopMovesCalculator implements ChessPieceMovesCalculator{
 
         Collection<ChessMove> possibleMoves = new ArrayList<>();
 
-        int currentRow = myPosition.getRow();
-        int currentCol = myPosition.getColumn();
+        int[][] directions = {
+                { 1, -1}, { 1, 1},
+                {-1, -1}, {-1, 1}
+        };
 
-        // bishops can move up and to the right
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition upRight = new ChessPosition(currentRow+i, currentCol+i);
-            ChessMove newMove = new ChessMove(myPosition, upRight);
-
-            if (isInBounds(upRight)) {
-                if (canCapture(board, myPosition, upRight)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, upRight)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // bishops can move up and to the left
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition upLeft = new ChessPosition(currentRow+i, currentCol-i);
-            ChessMove newMove = new ChessMove(myPosition, upLeft);
-
-            if (isInBounds(upLeft)) {
-                if (canCapture(board, myPosition, upLeft)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, upLeft)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // bishops can move down and to the right
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition downRight = new ChessPosition(currentRow-i, currentCol+i);
-            ChessMove newMove = new ChessMove(myPosition, downRight);
-
-            if (isInBounds(downRight)) {
-                if (canCapture(board, myPosition, downRight)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, downRight)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // bishops can move down and to the left
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition downLeft = new ChessPosition(currentRow-i, currentCol-i);
-            ChessMove newMove = new ChessMove(myPosition, downLeft);
-
-            if (isInBounds(downLeft)) {
-                if (canCapture(board, myPosition, downLeft)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, downLeft)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
+        for (int[] direction : directions) {
+            possibleMoves = continuousMoves(possibleMoves, board, myPosition, direction[0], direction[1]);
         }
 
         return possibleMoves;
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -8,83 +8,14 @@ public class RookMovesCalculator implements ChessPieceMovesCalculator{
 
         Collection<ChessMove> possibleMoves = new ArrayList<>();
 
-        int currentRow = myPosition.getRow();
-        int currentCol = myPosition.getColumn();
+        int[][] directions = {
+                          { 1, 0},
+                { 0, -1},          { 0, 1},
+                          {-1, 0}
+        };
 
-        // rooks can move up
-        for (int i = 1; i <= 8; i++) {
-            ChessPosition up = new ChessPosition(currentRow+i, currentCol);
-            ChessMove newMove = new ChessMove(myPosition, up);
-
-            if (isInBounds(up)) {
-                if (canCapture(board, myPosition, up)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, up)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // rooks can move to the right
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition right = new ChessPosition(currentRow, currentCol+i);
-            ChessMove newMove = new ChessMove(myPosition, right);
-
-            if (isInBounds(right)) {
-                if (canCapture(board, myPosition, right)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, right)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // rooks can move down
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition down = new ChessPosition(currentRow-i, currentCol);
-            ChessMove newMove = new ChessMove(myPosition, down);
-
-            if (isInBounds(down)) {
-                if (canCapture(board, myPosition, down)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, down)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
-        }
-
-        // rooks can move to the left
-        for (int i = 1; i <= 7; i++) {
-            ChessPosition left = new ChessPosition(currentRow, currentCol-i);
-            ChessMove newMove = new ChessMove(myPosition, left);
-
-            if (isInBounds(left)) {
-                if (canCapture(board, myPosition, left)) {
-                    possibleMoves.add(newMove);
-                    break;
-                }
-                else if (isBlocked(board, myPosition, left)) {
-                    break;
-                }
-                else {
-                    possibleMoves.add(newMove);
-                }
-            }
+        for (int[] direction : directions) {
+            possibleMoves = continuousMoves(possibleMoves, board, myPosition, direction[0], direction[1]);
         }
 
         return possibleMoves;
